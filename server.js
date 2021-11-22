f686a17494379957e8acacac6121ca840dd40a17
Michael Hamilton <hamiltonisaiah9119@gmail.com>
2021-11-22 16:52:27 -0700

    unblock cors

--- server.js
+++ server.js
@@ -2,12 +2,24 @@ require('dotenv').config();
 require('./db/mongoose')
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 9000;
 const cors = require('cors');
+const PORT = process.env.PORT || 9000;
 
 const comicController = require('./controllers/comicAPI')
 
-app.use(express.json())
-app.use("/comic", comicController)
+const urlArray = ['http://localhost:3000'];
+const corsOptions = {
+    origin: (origin, cb) => {
+        if (urlArray.indexOf(origin) != -1 || !origin) {
+            cb(null, true)
+        } else {
+            cb(new Error('Not allowed by CORS!!!'))
+        }
+    }
+}
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use("/comic", comicController);
 
 app.listen(PORT, () => console.log(`🕷 listening on PORT: ${PORT}`))
