1832f31540c7a6bc942e03da6bd58975879265b5
Michael Hamilton <hamiltonisaiah9119@gmail.com>
2021-11-22 00:22:25 -0700

    url for controller

--- server.js
+++ server.js
@@ -2,4 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 9000;
 
+const comic = require('./conrollers/comic')
+app.use('/comic', comic)
+
 app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`))
